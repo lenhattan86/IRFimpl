4bb84d2f3223b47bb2bd4ef917ac55673c01bc25
Tan N. Le <lenhattan86@gmail.com>
2018-01-26 10:19:46 -0500

    update

--- tests/baseline/runner.sh
+++ tests/baseline/runner.sh
@@ -6,7 +6,7 @@ mkdir jobs
 
 rm -rf logs/time.log
 
-userid="user1 user2";
+userid="user1 user2 user3";
 initTime=1
 
 sudo docker pull lenhattan86/bench
@@ -44,7 +44,7 @@ START=$(date +%s)
 
 user_func user1 2 1 3 2 "sleep 30" 5 &
 user_func user2 2 26 1 2 "sleep 30" 5 & 
-user_func user2 2 26 0 2 "sleep 30" 5 & 
+user_func user3 2 26 0 2 "sleep 30" 5 & 
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))

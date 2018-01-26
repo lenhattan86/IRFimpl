0dbedbc007bbae8c19ced5bb233a46b5fc0338dd
Tan N. Le <lenhattan86@gmail.com>
2018-01-26 10:14:01 -0500

    update

--- tests/baseline/runner.sh
+++ tests/baseline/runner.sh
@@ -42,8 +42,9 @@ user_func () {
 
 START=$(date +%s)
 
-user_func user1 2 4 0 2 "sleep 5" 5 &
-user_func user2 2 1 1 2 "sleep 5" 5
+user_func user1 2 1 3 2 "sleep 30" 5 &
+user_func user2 2 26 1 2 "sleep 30" 5 & 
+user_func user2 2 26 0 2 "sleep 30" 5 & 
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))

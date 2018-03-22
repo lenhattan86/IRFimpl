5ba24454ef78e0f25f5ffb537177534a2fe15456
Tan N. Le <lenhattan86@gmail.com>
2018-03-22 17:01:58 -0400

    regular update

--- experiments/profiling/alexnet/profiling.sh
+++ experiments/profiling/alexnet/profiling.sh
@@ -1,4 +1,6 @@
 kubectl delete pods --all --namespace=default 
+echo wait... 
+sleep 30 
 sudo docker pull lenhattan86/cpu 
 sudo docker pull lenhattan86/gpu 
 python ../../get_user_info.py --user=default --interval=1 --stopTime=10000 --file=alexnet.csv & pythonScript=$! 

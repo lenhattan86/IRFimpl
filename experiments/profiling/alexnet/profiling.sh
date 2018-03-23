3721e600a64a5674591d7758dc442b7c3e5051f9
Tan N. Le <lenhattan86@gmail.com>
2018-03-23 09:33:13 -0400

    regular update

--- experiments/profiling/alexnet/profiling.sh
+++ experiments/profiling/alexnet/profiling.sh
@@ -3,7 +3,7 @@ echo wait...
 sleep 90 
 sudo docker pull lenhattan86/cpu 
 sudo docker pull lenhattan86/gpu 
-python ../../get_user_info_timer.py --user=default --interval=1 --stopTime=10000 --file=alexnet.csv & pythonScript=$! 
+python ../../get_user_info.py --user=default --interval=1 --stopTime=10000 --file=alexnet.csv & pythonScript=$! 
 ./alexnet-cpu.sh & cpuScript=$! 
 ./alexnet-gpu.sh & gpuScript=$!
 wait

366e60491313bf51840d9bdbcc4126837cff13cc
Tan N. Le <lenhattan86@gmail.com>
2018-04-07 21:59:17 -0400

    regular update

--- experiments/cpu_on_gpu/alexnet/profiling.sh
+++ experiments/cpu_on_gpu/alexnet/profiling.sh
@@ -1,7 +1,7 @@
 kubectl delete pods --all --namespace=default 
 echo wait... 
-#sleep 60 
+sleep 60 
 sudo docker pull lenhattan86/gpu 
-python ../../get_user_info_timer.py --user=default --interval=1 --stopTime=10000 --file=alexnet.csv & pythonScript=$! 
-#./alexnet.sh & cpuScript=$! 
+python ../../get_user_info_timer.py  --interval=1 --stopTime=10000 --file=alexnet.csv & pythonScript=$! 
+./alexnet.sh & cpuScript=$! 
 wait

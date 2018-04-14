aaf4f7d9ac941af3e61ea28d17ca7e8c67fdab65
Tan N. Le <lenhattan86@gmail.com>
2018-04-14 16:20:24 -0400

    regular update

--- experiments/beta_motivation/profiling.sh
+++ experiments/beta_motivation/profiling.sh
@@ -2,6 +2,6 @@ kubectl delete pods --all --namespace=default --grace-period=0 --force
 echo wait... 
 sleep 60 
 sudo docker pull lenhattan86/gpu 
-python ../../get_user_info_timer.py  --interval=1 --stopTime=36000 --file=beta_mov.csv & pythonScript=$! 
+python ../get_user_info_timer.py  --interval=1 --stopTime=36000 --file=beta_mov.csv & pythonScript=$! 
 ./beta_mov.sh & cpuScript=$! 
 wait

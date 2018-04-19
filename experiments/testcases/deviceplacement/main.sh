63a4f90001d77595615c1ff75052e74ffd3b90a4
Tan N. Le <lenhattan86@gmail.com>
2018-04-19 13:51:51 -0400

    regular update

--- experiments/testcases/deviceplacement/main.sh
+++ experiments/testcases/deviceplacement/main.sh
@@ -1,10 +1,10 @@
 sudo docker pull lenhattan86/ira:cpu 
 sudo docker pull lenhattan86/ira:gpu 
 kubectl delete pod --all
-kubectl delete pod --all --namespace user1
-kubectl delete pod --all --namespace user2
+kubectl delete pod --all --namespace user1 --grace-period=0 --force 
+kubectl delete pod --all --namespace user2 --grace-period=0 --force 
 sleep 30
 kubectl create -f user1.yaml 
-python ../../get_user_info_timer.py  --interval=1 --stopTime=5250 --file=pods.csv & 
+python ../../get_user_info_timer.py  --interval=1 --stopTime=200 --file=pods.csv & 
 ./user1.sh
 wait

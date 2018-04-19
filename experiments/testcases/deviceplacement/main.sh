ee9947d7fdf93ed9efe0a095cbf375f7300a11d8
Tan N. Le <lenhattan86@gmail.com>
2018-04-19 11:50:08 -0400

    regular update

--- experiments/testcases/deviceplacement/main.sh
+++ experiments/testcases/deviceplacement/main.sh
@@ -4,6 +4,6 @@ kubectl delete pod --all
 kubectl delete pod --all --namespace user1
 kubectl delete pod --all --namespace user2
 kubectl create -f user1.yaml 
-python ../get_user_info_timer.py  --interval=1 --stopTime=5250 --file=pods.csv & 
+python ../../get_user_info_timer.py  --interval=1 --stopTime=5250 --file=pods.csv & 
 ./user1.sh
 wait

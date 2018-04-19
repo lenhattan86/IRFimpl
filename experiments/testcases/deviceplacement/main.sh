7fb0bde8fce5eb766d28985596e617d7d06355a6
Tan N. Le <lenhattan86@gmail.com>
2018-04-19 13:20:41 -0400

    regular update

--- experiments/testcases/deviceplacement/main.sh
+++ experiments/testcases/deviceplacement/main.sh
@@ -3,6 +3,7 @@ sudo docker pull lenhattan86/ira:gpu
 kubectl delete pod --all
 kubectl delete pod --all --namespace user1
 kubectl delete pod --all --namespace user2
+sleep 30
 kubectl create -f user1.yaml 
 python ../../get_user_info_timer.py  --interval=1 --stopTime=5250 --file=pods.csv & 
 ./user1.sh

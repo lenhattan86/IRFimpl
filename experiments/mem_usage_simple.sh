5b746f2e755e3499eed218f641824cfaafb739bc
Tan N. Le <lenhattan86@gmail.com>
2018-05-02 12:33:22 -0400

    regular update

--- experiments/mem_usage_simple.sh
+++ experiments/mem_usage_simple.sh
@@ -11,11 +11,13 @@ job="linear_regression"
 for batchSize in $BATCH_SIZES;
 do
     echo "============$batchSize ============"
+    device='cpu'
     rm -rf "$MEM_LOG/$job.$batchSize.$device.csv"
     ./measure_res_usage.sh "$MEM_LOG/$job.$batchSize.$device.csv" $STOP_TIME & logScript=$! 
     sleep 5
     # then start the jobs
-    python $BENCH_FOLDER/linear_regression.py --batch_size=$batchSize
+    # python $BENCH_FOLDER/linear_regression.py --batch_size=$batchSize
+    python ../../benchmarks/scripts/tf_benchmarks/linear_regression.py --batch_size=1024
     kill $logScript
     echo 'wait for OS to clean the memory'
     sleep 10        

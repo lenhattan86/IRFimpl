f239101b667746dd7e440c6acd1ca954b1eed777
Tan N. Le <lenhattan86@gmail.com>
2018-05-01 23:11:33 -0400

    regular update

--- experiments/mem_usage.sh
+++ experiments/mem_usage.sh
@@ -9,6 +9,7 @@ MEM_LOG="mem_usage"
 mkdir $MEM_LOG
 rm -rf $MEM_LOG/*
 STOP_TIME=10000
+NUM_BATCHES=5
 for job in $JOB_NAMES;
 do
     for batchSize in $BATCH_SIZES;
@@ -20,7 +21,7 @@ do
         ./measure_res_usage.sh "$MEM_LOG/$job.$batchSize.$device.csv" $STOP_TIME & logScript=$! 
         sleep 5
         # then start the jobs
-        python $BENCH_FOLDER/tf_cnn_benchmarks.py --device=gpu --num_warmup_batches=0  --model=$job --batch_size=$batchSize --num_batches=50
+        python $BENCH_FOLDER/tf_cnn_benchmarks.py --device=gpu --num_warmup_batches=0  --model=$job --batch_size=$batchSize --num_batches=$NUM_BATCHES
         kill $logScript
         echo 'wait for OS to clean the memory'
         sleep 10
@@ -31,7 +32,7 @@ do
         ./measure_res_usage.sh "$MEM_LOG/$job.$batchSize.$device.csv" $STOP_TIME & logScript=$! 
         sleep 5
         # then start the jobs
-        python $BENCH_FOLDER/tf_cnn_benchmarks.py --device=cpu --data_format=NHWC --num_warmup_batches=0  --model=$job --batch_size=$batchSize --num_intra_threads=19 --num_batches=50
+        python $BENCH_FOLDER/tf_cnn_benchmarks.py --device=cpu --data_format=NHWC --num_warmup_batches=0  --model=$job --batch_size=$batchSize --num_intra_threads=19 --num_batches=$NUM_BATCHES
         kill $logScript        
         echo 'wait for OS to clean the memory'
         sleep 10        

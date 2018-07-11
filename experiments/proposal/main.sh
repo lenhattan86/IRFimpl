f0bf2089e0d75df4819ffe0e98ca2f8e49324e63
Tan N. Le <lenhattan86@gmail.com>
2018-07-11 09:16:19 -0400

    regular update

--- experiments/proposal/main.sh
+++ experiments/proposal/main.sh
@@ -1,13 +1,14 @@
 sudo docker pull lenhattan86/ira:cpu 
 sudo docker pull lenhattan86/ira:gpu 
-kubectl delete pod --all --namespace user1
-kubectl delete pod --all --namespace user2
-kubectl delete pod --all --namespace user3
-echo wait.... ; sleep 60 
-kubectl create -f user1.yaml 
-kubectl create -f user2.yaml 
-kubectl create -f user3.yaml 
+kubectl delete pod --all
+
+kubectl create -f priority-40.yaml
+kubectl create -f priority-60.yaml
+kubectl create -f priority-120.yaml
+echo wait.... ; 
+sleep 10
 ./user1.sh &
 ./user2.sh &
 ./user3.sh &
-sleep 0; python ../get_user_info_timer.py  --interval=5 --stopTime=3000 --file=pods.csv 
+./user4.sh &
+sleep 0; python ../get_user_info_timer.py  --interval=1 --stopTime=1000 --file=pods.csv 
---
+++ experiments/proposal/user4.sh
@@ -0,0 +1,5 @@
+sleep 80
+kubectl create -f user4-0.yaml &
+kubectl create -f user4-1.yaml &
+kubectl create -f user4-2.yaml &
+kubectl create -f user4-3.yaml 

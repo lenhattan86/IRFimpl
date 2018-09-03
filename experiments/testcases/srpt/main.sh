bf9811b573eca2d0277ac3f5e38c956cad70ecf8
Tan N. Le <lenhattan86@gmail.com>
2018-09-03 15:59:35 -0400

    update testcase for srpt

--- experiments/testcases/srpt/main.sh
+++ experiments/testcases/srpt/main.sh
@@ -1,8 +1,7 @@
-sudo docker pull lenhattan86/ira:cpu 
-sudo docker pull lenhattan86/ira:gpu 
+# sudo docker pull lenhattan86/ira:cpu 
+# sudo docker pull lenhattan86/ira:gpu 
 kubectl delete pod --all
 kubectl delete pod --all --namespace user1 --grace-period=0 --force 
-kubectl delete pod --all --namespace user2 --grace-period=0 --force 
 kubectl create -f user1.yaml 
 #python ../../get_user_info_timer.py  --interval=1 --stopTime=200 --file=pods.csv & 
 ./user1.sh
--- experiments/testcases/srpt/user1.sh
+++ experiments/testcases/srpt/user1.sh
@@ -1,6 +1,6 @@
 kubectl --namespace="user1" create -f user1-0.yaml 2> user1-0.log & 
 kubectl --namespace="user1" create -f user1-1.yaml 2> user1-1.log & 
-sleep 30
+sleep 15
 kubectl --namespace="user1" create -f user1-2.yaml 2> user1-2.log & 
 sleep 5
 kubectl --namespace="user1" create -f user1-3.yaml 2> user1-3.log & 

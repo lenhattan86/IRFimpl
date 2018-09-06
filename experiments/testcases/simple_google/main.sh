8a074ef28cc09886fc891875ec7d34f6458f7395
Tan N. Le <lenhattan86@gmail.com>
2018-09-06 13:14:54 -0400

    update simple google

--- experiments/testcases/simple_google/main.sh
+++ experiments/testcases/simple_google/main.sh
@@ -5,7 +5,7 @@ kubectl delete pod --all --namespace user1 --grace-period=0 --force
 kubectl delete pod --all --namespace user2 --grace-period=0 --force 
 kubectl create -f user1.yaml 
 kubectl create -f user2.yaml 
-#python ../../get_user_info_timer.py  --interval=1 --stopTime=200 --file=pods.csv & 
+python ../../get_user_info_timer.py  --interval=1 --stopTime=200 --file=pods.csv & 
 ./user1.sh &
 ./user2.sh &
 wait

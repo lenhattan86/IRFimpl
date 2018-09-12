803e5286629a141ddbbea27ab339f89d4c8206bd
Tan N. Le <lenhattan86@gmail.com>
2018-09-11 20:00:22 -0400

    update simple tf gpu

---
+++ experiments/testcases/simple_tf_gpu/user1.sh
@@ -0,0 +1,14 @@
+kubectl --namespace="user1" create -f user1-0.yaml 2> user1-0.log & 
+kubectl --namespace="user1" create -f user1-1.yaml 2> user1-1.log & 
+sleep 1
+kubectl --namespace="user1" create -f user1-2.yaml 2> user1-2.log & 
+kubectl --namespace="user1" create -f user1-3.yaml 2> user1-3.log & 
+sleep 1
+kubectl --namespace="user1" create -f user1-4.yaml 2> user1-4.log & 
+kubectl --namespace="user1" create -f user1-5.yaml 2> user1-5.log & 
+sleep 1
+kubectl --namespace="user1" create -f user1-6.yaml 2> user1-6.log & 
+kubectl --namespace="user1" create -f user1-7.yaml 2> user1-7.log & 
+sleep 1
+kubectl --namespace="user1" create -f user1-8.yaml 2> user1-8.log & 
+kubectl --namespace="user1" create -f user1-9.yaml 2> user1-9.log & 

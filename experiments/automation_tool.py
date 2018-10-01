f7b045d6d9400746913d642ea48d55cfa1540fc1
Tan N. Le <lenhattan86@gmail.com>
2018-10-01 02:16:28 -0400

    regular update

--- experiments/automation_tool.py
+++ experiments/automation_tool.py
@@ -547,7 +547,7 @@ def main():
                 thread4.start()         
                 gpuShortJobs_2[jobIdKey] = newJob 
                 
-                sleep(interval) # give some time for kubernetes
+                # sleep(interval) # give some time for kubernetes
                 ###################################################################
                 pendingJobs, startedJobs, completedJobs, currTime = listJobStatus()                
                 updateJobInfo(startedJobs, completedJobs, cpuShortJobs_1, currTime)
